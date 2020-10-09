1d57dbd0f0091447ee5651c978f1afa3539675d9
Michael Levan <mlevan1992@gmail.com>
2020-10-09 06:03:43 -0400

    clientID

--- AKS-Create/__main__.py
+++ AKS-Create/__main__.py
@@ -2,8 +2,8 @@
 # pulumi config set --secret clientSecret your_secret
 # pulumi config set clientID your_client_id
 
-import pulumi
 import logging
+import pulumi
 import aksParamConstants as constants
 from pulumi_azure import containerservice, core
     
@@ -11,25 +11,29 @@ from pulumi_azure import containerservice, core
 def createAKSCluster():
     config = pulumi.Config()
 
-    try:
-        containerservice.KubernetesCluster(
-            resource_name = constants.name,
-            default_node_pool={
-                'min_count': constants.min_count,
-                'max_count': constants.max_count,
-                'name': constants.name,
-                'vm_size': constants.vm_size,
-                'enable_auto_scaling': constants.auto_scaling
-            },
-            dns_prefix=constants.dns_prefix,    
-            resource_group_name=constants.resource_group_name,
-            service_principal={
-                'client_id': config.require('clientID'),
-                'client_secret': config.require_secret('clientSecret')
-                }
-            )
+    if containerservice == None:
+        logging.warning("Check to ensure the containerservice import is accurate")
+    
+    else:
+        try:
+            containerservice.KubernetesCluster(
+                resource_name = constants.name,
+                default_node_pool={
+                    'min_count': constants.min_count,
+                    'max_count': constants.max_count,
+                    'name': constants.name,
+                    'vm_size': constants.vm_size,
+                    'enable_auto_scaling': constants.auto_scaling
+                },
+                dns_prefix=constants.dns_prefix,    
+                resource_group_name=constants.resource_group_name,
+                service_principal={
+                    'client_id': constants.clientID,
+                    'client_secret': config.require_secret('clientSecret')
+                    }
+                )
 
-    except Exception as e:
-        logging.error(e)
+        except Exception as e:
+            logging.error(e)
 
 createAKSCluster()
